083c36766884a9a304ea1ebb3a8460fa595d4574
Igor <devNoon@yandex.ru>
2022-04-14 02:09:59 +0300

    prepare for deployment i++

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ import AuthController from "./controllers/AuthController";
 import MessageController from "./controllers/MessageController";
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const server = require("http").Server(app);
 
