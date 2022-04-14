a5ec0acbe0d52b37eb84c90bd1b01a8b202ab0de
Igor <devNoon@yandex.ru>
2022-04-14 03:09:59 +0300

    ///

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME!,
+  process.env.DB_USERNAME!,
+  process.env.DB_PASSWORD!,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST!,
     dialect: "postgres",
     logging: false,
     dialectOptions: {
